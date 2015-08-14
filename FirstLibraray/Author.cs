e899a7c096c5e99d912ff9dee87e538ab106342b
idonald <idonald@DLT3ZYHWW1.ad.laterooms.com>
2015-08-14 16:19:02 +0100

    Update All

---
+++ FirstLibraray/Author.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstLibraray
+{
+    public class Author
+    {
+       public int ID;
+       public string Name;
+
+        public Author(int id, string name = "")
+        {
+            ID = id;
+            Name = name;
+        }
+    }
+}
---
+++ UnitTests/AuthorTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FirstLibraray;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class AuthorTest
+    {
+        [TestMethod]
+        public void CreateAuthor()
+        {
+            var blarg = new Author(1);
+            Assert.IsInstanceOfType(blarg, typeof(Author));
+            Assert.AreEqual(blarg.ID, 1);
+        }
+
+        [TestMethod]
+        public void CreateAuthorWithName()
+        {
+            var blarg = new Author(1, "Dave");
+            Assert.IsInstanceOfType(blarg, typeof(Author));
+            Assert.AreEqual(blarg.Name, "Dave");
+        }
+    }
+}
